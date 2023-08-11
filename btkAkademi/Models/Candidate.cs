fa86a70f9f8a2c2621b3d45b0992cdd8be2247ba
Minealtug <200316021@ogr.cbu.edu.tr>
2023-08-11 12:43:35 +0300

    Proje Oluşturma ve Model

---
+++ btkAkademi/Models/Candidate.cs
@@ -0,0 +1,25 @@
+﻿namespace btkAkademi.Models
+{
+    public class Candidate
+    {
+        public string? Email { get; set; } = string.Empty;
+
+        public string? FirstName { get; set; } = string.Empty;
+
+        public string? Lastname { get; set; } = string.Empty;
+
+        public string? FullName => $"{FirstName} {Lastname?.ToUpper()}";
+
+        public int? Age { get; set; };
+
+        public string? SelectedCourse { get; set; } = string.Empty;
+
+
+        public dateTime ApplyAt { get; set; };
+
+        public Candidate 
+        {
+        ApplyAt=DateTime.Now;
+        }
+    }
+}
